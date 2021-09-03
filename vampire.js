ef396288226ddb50bdda9acc4100c948d36dba86
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-09-03 15:43:41 -0700

    Intro to Trees: Vampr

--- vampire.js
+++ vampire.js
@@ -10,21 +10,49 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let vampiresAway = 0;
+    let vampireThis = this;
 
+    while (vampireThis.creator) {
+      vampireThis = vampireThis.creator;
+      vampiresAway++;
+    }
+
+    return vampiresAway;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
+
+  }
+
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
+
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
 
   }
 
@@ -41,4 +69,3 @@ class Vampire {
 }
 
 module.exports = Vampire;
-
